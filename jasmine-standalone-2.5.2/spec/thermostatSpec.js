49ea062356f86d7d68589accc62a63d9c72a8b0f
ajbater <amanda22@hotmail.co.uk>
2016-12-07 17:26:24 +0000

    temperature starts at 20 degrees

---
+++ jasmine-standalone-2.5.2/spec/thermostatSpec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+describe('Thermostat', function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('should start at 20 degrees', function() {
+    expect(thermostat._temperature).toEqual(20);
+  });
+});
